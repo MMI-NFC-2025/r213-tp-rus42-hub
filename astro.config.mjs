b5f73431695a104693bcc3ee45ee5e5972f3b577
rus42-hub <ruslana.budniak@edu.univ-fcomte.fr>
2026-02-06 08:35:07 +0100

    Enable server output for dynamic routes

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,12 @@
 import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
-
 import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+
   vite: {
     plugins: [tailwindcss()]
   },
