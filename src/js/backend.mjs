874a4eeaa6e56c43463487dcdd91226d3c397f29
rus42-hub <ruslana.budniak@edu.univ-fcomte.fr>
2026-02-06 09:00:54 +0100

    Add PocketBase integration and create dynamic offer detail page

---
+++ src/js/backend.mjs
@@ -0,0 +1,29 @@
+import PocketBase from 'pocketbase';
+
+const db = new PocketBase('http://127.0.0.1:8090');
+
+export async function getOffres() {
+    try {
+        const data = await db.collection('maison').getFullList({
+            sort: '-created',
+        });
+        return data;
+    } catch (error) {
+        console.log('Une erreur est survenue en lisant la liste des maisons', error);
+        return [];
+    }
+}
+
+export async function getOffre(id) {
+    try {
+        const data = await db.collection('maison').getOne(id);
+        return data;
+    } catch (error) {
+        console.log('Une erreur est survenue en lisant la maison', error);
+        return null;
+    }
+}
+
+export async function getImageUrl(record, recordImage) {
+    return db.files.getURL(record, recordImage);
+}
